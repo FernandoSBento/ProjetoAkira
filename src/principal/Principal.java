f5f715158a0bf6a83a8a5456e7db1568f5591da4
Fernando <fernando_bento2@hotmail.com>
2018-09-13 22:04:24 -0300

    update projeto no git

---
+++ src/principal/Principal.java
@@ -0,0 +1,60 @@
+package principal;
+
+import dao.DAOGenerico;
+import entidades.Jogos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+//		Cliente cli = new Cliente();
+//		DAOGenerico<Cliente> daoCliente = new DAOGenerico<>(Cliente.class);
+//
+//		
+//		cli.setNome("Joao");
+//		
+//		daoCliente.salvar(cli);
+		
+//		Genero ge = new Genero();
+//		DAOGenerico<Genero> dg = new DAOGenerico<>(Genero.class);
+//		
+//		ge.setNome("RPG");
+//	
+//		dg.salvar(ge);
+		
+		
+//		Produtora pro = new Produtora();
+//		DAOGenerico<Produtora> daop = new DAOGenerico<>(Produtora.class);
+//		
+//		pro.setNome("EA");
+//		
+//		daop.salvar(pro);
+		
+		Jogos jg = new Jogos();
+		DAOGenerico<Jogos> djogos = new DAOGenerico<>(Jogos.class);
+//		
+//		jg.setTitulo("GTA V");
+//		jg.setIdadeMinima(16);
+//		jg.setProdutora(null);
+//		jg.setGenero(null);
+//		jg.setStatus(null);
+//		jg.setPlataforma(null);
+//		jg.setAno(2018);
+//		jg.setMultiplayer(true);
+//		jg.setEhOnline(true);
+//		
+//		djogos.salvar(jg);
+		
+//		Obra o = new Obra();
+//		DAOGenerico<Obra> d = new DAOGenerico<>(Obra.class);
+//		
+//		o.setTitulo("God Of War");
+//		o.setAno(2018);
+//		o.setIdadeMinima(18);
+//		o.setStatus(1);
+//		o.setProdutora("RPG");
+//		o.setGenero(genero);
+//		d.salvar(o);
+			
+	}
+}
